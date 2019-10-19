bb496b4ed25c52ec8c4c2247354edf4a61b4fde1
AverageName <kotemax3@mail.ru>
2019-10-19 19:41:40 +0300

    I tak soidet

--- script.py
+++ script.py
@@ -24,9 +24,11 @@ def get_value_by_key(data, key):
 def save_plot(data, path, name, label, x_scale=1):
     plt.figure()
     ax = sns.distplot(data * x_scale, hist=False, kde=True, axlabel=label)
+    ax.set_yticklabels([])
     filename = name + ".jpg"
     plt.savefig(filename)
     os.replace(os.path.join('.', filename), os.path.join(path, filename))
+
 
 #  Parser for script.    
 parser = argparse.ArgumentParser(description="Script for CPUWallTime Graphs")
@@ -34,6 +36,7 @@ parser.add_argument("--csv_file", dest="csv_file", type=str, help="name of the c
 parser.add_argument("--root_dir", dest="root_dir", type=str, help="path to the root diretory")
 parser.add_argument("--w_l", dest="w_l", type=int, help="walltime low")
 parser.add_argument("--w_h",dest="w_h", type=int, help="walltime high")
+parser.add_argument("--scale", dest="scale", type=float, help="scale of time axis for walltime duration ")
 parser.add_argument("--num_cpu_l", dest="num_cpu_l", type=int, help="lowest number of used cpus")
 parser.add_argument("--num_cpu_h", dest="num_cpu_h", type=int, help="highest number of used cpus")
 parser.add_argument("--num_nodes_l", dest="num_nodes_l", type=int, help="lowest number of used nodes")
@@ -47,7 +50,7 @@ args = parser.parse_args()
 data = read_csv(args.root_dir, args.csv_file)
 data = filter_data(data, {"WallDurationSeconds": [args.w_l, args.w_h], "NumberCPU":[args.num_cpu_l, args.num_cpu_h], 
                                                            "Nodes":[args.num_nodes_l, args.num_nodes_h], "QueueName": args.queue_name})
-save_plot(get_value_by_key(data, "WallDurationSeconds"), "./graph", "wall_duration_plot", "WallDurationSeconds", 1/60)
+save_plot(get_value_by_key(data, "WallDurationSeconds"), "./graph", "wall_duration_plot", "WallDurationSeconds", args.scale)
 save_plot(get_value_by_key(data, "NumberCPU"), "./graph", "cpu_plot", "NumberCPU")
 save_plot(get_value_by_key(data, "Nodes"), "./graph", "nodes_plot", "Nodes")
 
