981c1fc521177a8e7a97b88a68f92e31e6a817b0
AverageName <kotemax3@mail.ru>
2019-10-29 00:12:43 +0300

    Refactored, added definition of params by yaml file

--- script.py
+++ script.py
@@ -4,6 +4,12 @@ import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
+import yaml
+
+def parse_yaml(yaml_path):
+    stream = open(yaml_path, "r")
+    data = yaml.safe_load(stream)
+    return data
 
 def read_csv(root_dir, csv_file):
     data = pd.read_csv(os.path.join(root_dir, csv_file), delimiter=';')
@@ -29,28 +35,16 @@ def save_plot(data, path, name, label, x_scale=1):
     plt.savefig(filename)
     os.replace(os.path.join('.', filename), os.path.join(path, filename))
 
-
 #  Parser for script.    
 parser = argparse.ArgumentParser(description="Script for CPUWallTime Graphs")
-parser.add_argument("--csv_file", dest="csv_file", type=str, help="name of the csv_file")
-parser.add_argument("--root_dir", dest="root_dir", type=str, help="path to the root diretory")
-parser.add_argument("--w_l", dest="w_l", type=int, help="walltime low")
-parser.add_argument("--w_h",dest="w_h", type=int, help="walltime high")
-parser.add_argument("--scale", dest="scale", type=float, help="scale of time axis for walltime duration ")
-parser.add_argument("--num_cpu_l", dest="num_cpu_l", type=int, help="lowest number of used cpus")
-parser.add_argument("--num_cpu_h", dest="num_cpu_h", type=int, help="highest number of used cpus")
-parser.add_argument("--num_nodes_l", dest="num_nodes_l", type=int, help="lowest number of used nodes")
-parser.add_argument("--num_nodes_h", dest="num_nodes_h", type=int, help="highest number of used nodes")
-parser.add_argument("--queue_name", dest="queue_name", nargs="+", help="queue type")
-parser.add_argument("--dest_path", dest="dest_path", type=str, help="destination path for graphs")
-
+parser.add_argument("--yaml_path", dest="yaml_path", type=str, help="path to yaml config file")
 
 args = parser.parse_args()
 
-data = read_csv(args.root_dir, args.csv_file)
-data = filter_data(data, {"WallDurationSeconds": [args.w_l, args.w_h], "NumberCPU":[args.num_cpu_l, args.num_cpu_h], 
-                                                           "Nodes":[args.num_nodes_l, args.num_nodes_h], "QueueName": args.queue_name})
-save_plot(get_value_by_key(data, "WallDurationSeconds"), "./graph", "wall_duration_plot", "WallDurationSeconds", args.scale)
-save_plot(get_value_by_key(data, "NumberCPU"), "./graph", "cpu_plot", "NumberCPU")
-save_plot(get_value_by_key(data, "Nodes"), "./graph", "nodes_plot", "Nodes")
+params = parse_yaml(args.yaml_path)
+data = read_csv(params["root_dir"], params["csv_file"])
+data = filter_data(data, params["filters"])
+save_plot(get_value_by_key(data, "WallDurationSeconds"), params["dest_path"], "wall_duration_plot", "WallDurationSeconds")
+save_plot(get_value_by_key(data, "NumberCPU"), params["dest_path"], "cpu_plot", "NumberCPU")
+save_plot(get_value_by_key(data, "Nodes"), params["dest_path"], "nodes_plot", "Nodes")
 
