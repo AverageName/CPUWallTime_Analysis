432d0434e01ea8496dc617fcd61267a1870a468e
AverageName <max.pakhomov@mail.ru>
2019-11-21 15:28:52 +0300

    "Fixed almost all bugs"

--- script.py
+++ script.py
@@ -1,7 +1,6 @@
 import os
 import csv
 import pandas as pd
-import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
@@ -23,11 +22,13 @@ def read_csv(root_dir, csv_file):
 
 
 def filter_data(data, params):
-    for key in params.keys():
-        if type(params[key][0]) == int:
-            data = data.loc[(data[key] >= params[key][0]) & (data[key] <= params[key][1])]
-        elif type(params[key][0]) == str:
-            data = data.loc[(data[key].isin(params[key]))]
+    if params is not None:
+        for key in params.keys():
+            if type(params[key][0]) in [int, float]:
+                data = data.loc[(data[key] >= params[key][0]) & (data[key] <= params[key][1])]
+                print(key, params[key][0], params[key][1])
+            elif type(params[key][0]) == str:
+                data = data.loc[(data[key].isin(params[key]))]
     return data
 
 
@@ -35,30 +36,38 @@ def get_value_by_key(data, key):
     return np.array(data[key].to_list())
 
 
-def save_plot_get_values(data, name, path, x_scale):
-    fig, ax = plt.subplots()
-    print(1.06 * np.std(data) * (data.shape[0] ** (-1/5)) + 1e-7)
-    if data.shape[0] == 1:
-        model = KernelDensity(bandwidth=1, kernel="gaussian")
-    else:
-        model = KernelDensity(bandwidth=1.06 * np.std(data) * (data.shape[0] ** (-1/5)) + 1e-6, kernel="gaussian")
-    model.fit(data.reshape(-1, 1))
-    values = np.array(list(range(np.min(data) - 10, np.max(data) + 10, 1)))
-    values = values.reshape(-1, 1)
-    probs = np.exp(model.score_samples(values))
-    ax.plot(values, probs)
-    #ax.set_yticklabels([])
-    plt.show()
-    #ax = sns.distplot(data / x_scale, hist=False, kde=True, axlabel=name)
-    #ax.set_yticklabels([])
-
+def save_jpg(name, path):
     filename = name + '.jpg'
     plt.savefig(filename)
     os.replace(os.path.join('.', filename), os.path.join(path, filename))
 
-    xs = ax.get_lines()[0].get_data()[0]
-    ys = ax.get_lines()[0].get_data()[1]
-    return [xs, ys]
+
+def save_plot_get_values(data, name, path, x_scale):
+    fig, ax = plt.subplots()
+    if data.shape[0] == 0:
+        print("No data with this parameters, try another configuration")
+        sys.exit(0)
+    if params["plot_type"] == "str":
+        plt.hist(data)
+        plt.show()
+        save_jpg(name, path)
+    else:
+        data = data / x_scale
+        if data.shape[0] == 1:
+            model = KernelDensity(bandwidth=1, kernel="gaussian")
+        else:
+            model = KernelDensity(bandwidth=1.06 * np.std(data) * (data.shape[0] ** (-1/5)) + 1e-6, kernel="gaussian")
+        model.fit(data.reshape(-1, 1))
+        values = np.linspace(np.min(data) - 2, np.max(data) + 2, 100)
+        values = values.reshape(-1, 1)
+        probs = np.exp(model.score_samples(values))
+        ax.plot(values, probs)
+        ax.set_yticklabels([])
+        plt.show()
+        save_jpg(name, path)
+        xs = ax.get_lines()[0].get_data()[0]
+        ys = ax.get_lines()[0].get_data()[1]
+        return [xs, ys]
 
 
 def save_values(xs, ys, name, table_path):
@@ -80,8 +89,6 @@ args = parser.parse_args()
 params = parse_yaml(args.yaml_path)
 data = read_csv(params["root_dir"], params["csv_file"])
 data = filter_data(data, params["filters"])
-print(data)
-print(data["WallDurationSeconds"].to_list())
 plot_name = params["plot_name"]
 
 #try:
@@ -89,6 +96,6 @@ xs_ys = save_plot_get_values(get_value_by_key(data, plot_name), plot_name, param
 #except ValueError as e:
 #    print('Value Error')
 #   sys.exit(0)
-
-save_values(xs_ys[0], xs_ys[1], plot_name, params["tables_path"])
+if params["plot_type"] != "str":
+    save_values(xs_ys[0], xs_ys[1], plot_name, params["tables_path"])
 print("Time: ", time.time() - start)
